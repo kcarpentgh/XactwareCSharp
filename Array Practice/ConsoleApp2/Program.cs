1144b9caa17a6ea4c61d00ebbb82d26b139e2174
kcarpentgh <kcarpenter@xactware.com>
2017-04-16 19:34:37 -0600

    complete

---
+++ Array Practice/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new[] { 5, 10, 15, 20, 25, 30, 35, 40, 45, 99 };
+            int length = array1.Length;
+            int[] array2 = new int [length];
+
+            for(int x = 0; x < length; x++)
+            {
+                array2[x] = array1[x];
+            }
+
+            for (int y = 0; y < length; y++)
+            {
+                Console.WriteLine("Value from Array 1: " + array1[y]);
+                Console.WriteLine("Value from Array 2: " + array2[y]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
