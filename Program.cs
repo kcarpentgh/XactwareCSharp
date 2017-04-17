fde1e03a018d6c713adb2976a398472f0d580eea
kcarpentgh <kcarpenter@xactware.com>
2017-04-16 18:57:53 -0600

    not working

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double v = 0; //volume
+            double sa = 0; //surface area
+            double pi = 3.1415926; //pi
+
+            Console.WriteLine("Please input the radius of the cylinder."); 
+            string UserInputRadius = Console.ReadLine(); //getting the radius from the user
+            double r = Convert.ToInt32(UserInputRadius);
+
+            Console.WriteLine("Please input the height of the cylinder.");
+            string UserInputHeight = Console.ReadLine(); //getting the height from the user
+            double h = Convert.ToInt32(UserInputHeight);
+
+            v = (pi * ((r * r) * h));
+
+            sa = ((2 * pi * r) * (r + h));
+
+            Console.WriteLine("The volume of the cylinder is: " + v);
+            Console.WriteLine("");
+            Console.WriteLine("The surface area of the cylinder is: " + sa);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Pyramid/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the start.");
+            for(int row = 0; row == 5; row++)
+            {
+                Console.WriteLine("1");
+                Console.WriteLine($"Loop {row}");
+
+                /*for(int column = 0; column == 9; column++)
+                {
+                    Console.WriteLine($"Loop {column}");
+                    if (row == 0 && column == 5)
+                        Console.Write("*");
+
+                        else if(row == 1 && (column >= 4 && column <= 6))
+                            Console.Write("*");
+                        else if(row == 2 && (column >= 3 && column <= 7))
+                            Console.Write("*");
+                        else if(row == 3 && (column >= 2 && column <= 8))
+                            Console.Write("*");
+                        else if(row == 4 && (column >= 1 && column <= 9))
+                            Console.Write("*");
+                        else if(row == 5 && (column >= 0 && column <= 10))
+                            Console.Write("*");
+                    else Console.Write(" ");
+
+                    Console.WriteLine();
+                    
+                }*/
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
